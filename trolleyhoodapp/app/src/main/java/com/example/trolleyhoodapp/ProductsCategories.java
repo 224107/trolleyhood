973c82adf0fc62c1055c78006e5a49e089ded9f8
224107 Zaleski Oskar <224107@edu.p.lodz.pl>
2020-11-26 12:21:35 +0100

    images added

---
+++ trolleyhoodapp/app/src/main/java/com/example/trolleyhoodapp/ProductsCategories.java
@@ -0,0 +1,14 @@
+package com.example.trolleyhoodapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProductsCategories extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
